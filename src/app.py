9bd20f337eab913dbd3dbde66951eda15ec24f1d
Jesper Martinsson <jesper.martinsson92@gmail.com>
2024-06-09 21:15:00 +0200

    pool temp

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from uuid import uuid1
 from flask import request
 
-from endpoints.pool import Pool_heating_on, Pool_heating_off, Init_GPIO
+from endpoints.pool import Pool_heating_on, Pool_heating_off, Init_GPIO, read_temp
 #from bson import ObjectId
 from bson.json_util import dumps, RELAXED_JSON_OPTIONS
 from bson.objectid import ObjectId
@@ -28,6 +28,10 @@ try:
     @app.get("/pool_off")
     def pool_off():
         return Pool_heating_off()
+    
+    @app.get("/pool_temp")
+    def pool_temp():
+        return read_temp()
 
     if __name__=="__main__":
         app.run(port=8887, debug=True)
--- src/endpoints/pool.py
+++ src/endpoints/pool.py
@@ -5,9 +5,18 @@ from bson import ObjectId
 from bson.json_util import dumps, RELAXED_JSON_OPTIONS
 from bson.objectid import ObjectId
 
+import os
+
 import time
 
 import RPi.GPIO as GPIO
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
 
 in1 = 11
 in2 = 13
@@ -58,4 +67,21 @@ def Pool_heating_off():
     else:
         print("Heating is already OFF")
         return "Pool Heating already OFF"
+    
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
     
