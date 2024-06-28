ffc329bb403443f9ac896a056ae0740b56d4d9e6
Jesper Martinsson <jesper.martinsson92@gmail.com>
2024-06-28 13:24:50 +0200

    removed logger from handler

--- src/endpoints/pool.py
+++ src/endpoints/pool.py
@@ -2,10 +2,6 @@ from flask import Flask
 
 import glob
 
-from bson import ObjectId
-from bson.json_util import dumps, RELAXED_JSON_OPTIONS
-from bson.objectid import ObjectId
-
 
 from datetime import datetime
 
@@ -15,7 +11,6 @@ import time
 
 import RPi.GPIO as GPIO
 
-from checkLoop import logger
 
 in1 = 11
 in2 = 13
@@ -41,7 +36,7 @@ def Reset_state():
     state = f.read()
     if state == "on":
         Heating = True
-        logger.info("Resetting state to ON")
+        #logger.info("Resetting state to ON")
         GPIO.output(in3, True)
 
 def Write_state(state):
@@ -52,35 +47,35 @@ def power_on():
     print("turning power on")
     GPIO.output(in2, True)
     time.sleep(180)
-    logger.info("turning power off")
+    #logger.info("turning power off")
     GPIO.output(in2, False)
 
 def Pool_heating_on():
     global Heating
     if Heating == False:
         Heating = True
-        logger.info("Pool heating on")
+        #logger.info("Pool heating on")
         Write_state("on")
         GPIO.output(in1, True)
         GPIO.output(in3, True)
         power_on()
         return "Pool Heating ON"
     else:
-        logger.info("Heating already ON")
+        #logger.info("Heating already ON")
         return "Pool Heating already On"
     
 def Pool_heating_off():
     global Heating
     if Heating == True:
         Heating = False
-        logger.info("pool heating off")
+        #logger.info("pool heating off")
         Write_state("off")
         GPIO.output(in1, False)
         GPIO.output(in3, False)
         power_on()
         return "Pool Heating OFF"
     else:
-        logger.info("Heating is already OFF")
+        #logger.info("Heating is already OFF")
         return "Pool Heating already OFF"
     
     
