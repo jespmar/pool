646640ef19096d948530d2388a827ed3f1f120d2
Jesper Martinsson <jesper.martinsson92@gmail.com>
2024-06-28 13:27:23 +0200

    more

--- src/checkLoop.py
+++ src/checkLoop.py
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(filename='myapp.log', level=logging.INFO)
 logger.info("Python Pool Temp Service is running")
 
-from endpoints.pool import Pool_heating_on, Pool_heating_off, Init_GPIO, read_temp, Write_state, Reset_state
+from endpoints.pool import Pool_heating_on, Pool_heating_off, Init_GPIO, Write_state, Reset_state
 db=Connection('pool_temp_test')
 
 Init_GPIO()
